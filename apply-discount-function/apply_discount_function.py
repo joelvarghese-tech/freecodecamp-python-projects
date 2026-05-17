9b7cd554db46737dd933d425653b0d39aaf42101
Joel Varghese <joelvarghese.tech@gmail.com>
2026-05-17 12:21:45 +0530

    Create apply_discount_function.py
    Implement apply_discount function with validation checks.

---
+++ apply-discount-function/apply_discount_function.py
@@ -0,0 +1,16 @@
+def apply_discount(price,discount):
+    if not isinstance(price,(int,float)):
+        return("The price should be a number")
+    elif not isinstance(discount,(int,float)):
+        return("The discount should be a number")
+    elif price <= 0:
+        return("The price should be greater than 0")
+    elif discount <0 or discount >100:
+        return("The discount should be between 0 and 100")
+    return(price-((discount/100)*price))
+
+print(apply_discount(100,20))
+print(apply_discount(200,50))
+print(apply_discount(50,0))
+print(apply_discount(100,100))
+print(apply_discount(74.5, 20.0))
